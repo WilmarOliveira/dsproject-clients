f39895b6ce7c534787c42e1f930a839093dc61b5
Wilmar Oliveira <wilmar_oliveira13@hotmail.com>
2021-01-27 20:52:52 -0300

    Service, resource and Config Properties

--- backend/src/main/java/com/devsuperior/dsproject/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dsproject/resources/ClientResource.java
@@ -1,26 +1,27 @@
 package com.devsuperior.dsproject.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsproject.entities.Client;
+import com.devsuperior.dsproject.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
+	
+	@Autowired
+	private ClientService service;
 
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll() {
 		
-		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "teste1", "10000000000", 1250.00, 2));
-		list.add(new Client(2L, "teste2", "20000000000", 2250.00, 3));
-		list.add(new Client(3L, "teste3", "30000000000", 3250.00, 4));
+		List<Client> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 		
