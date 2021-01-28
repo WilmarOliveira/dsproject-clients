1f0a1d1772f55b90547c58e211e022782760aaa2
Wilmar Oliveira <wilmar_oliveira13@hotmail.com>
2021-01-28 19:52:38 -0300

    DTO creation and Seeding Initial Database

--- backend/src/main/java/com/devsuperior/dsproject/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsproject/services/ClientService.java
@@ -1,10 +1,13 @@
 package com.devsuperior.dsproject.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dsproject.dto.ClientDTO;
 import com.devsuperior.dsproject.entities.Client;
 import com.devsuperior.dsproject.repositories.ClientRepository;
 
@@ -14,7 +17,12 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	public List<Client> findAll() {
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		
+		List<ClientDTO> listDto = list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+		
+		return listDto;
 	}
 }
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bruna', '25525525500', 1250.00, '1999-07-02', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Wilmar', '26626626600', 3250.00, '1990-08-03', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lourdes', '27727727700', 1150.00,'1964-01-02', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Wilmar', '28828828800', 3550.00, '1963-04-19', 2);
+	
